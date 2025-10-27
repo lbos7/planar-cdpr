b88961f4e841ddba912f74193f88fd1e7d7e3695
Logan Boswell <loganstuartboswell@gmail.com>
2025-10-27 12:27:38 -0500

    Got ros node to run correctly & plot; tag issues
    - created symlink for cdpr_serial file
    - updated config and launch files
    - updated setup.py so the config and launch files could be found in the correct places

--- cdpr_vision/cdpr_vision/tracker.py
+++ cdpr_vision/cdpr_vision/tracker.py
@@ -3,31 +3,31 @@ from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSDurabilityPolicy
 from geometry_msgs.msg import TransformStamped
 from std_srvs.srv import Empty
+from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
-from cdpr_msgs.srv import Waypoint, ActivateTrajWaypoints, Move, MoveTraj
+from cdpr_msgs.srv import Waypoint, ActivateTrajWaypoints, Move, TrajMove
 
 import numpy as np
 import matplotlib.pyplot as plt
 import serial
 import threading
-import time
 from collections import deque
-from cdpr_utils.cdpr_serial import serial_read_thread, serial_write
+from cdpr_vision.cdpr_serial import serial_read_thread, serial_write
 
 
 class Tracker(Node):
 
     def __init__(self):
-        super().__init__("Tracker")
+        super().__init__("tracker")
         qos = QoSProfile(depth=10,
                          durability=QoSDurabilityPolicy.TRANSIENT_LOCAL)
 
-        self.timer = self.create_timer(1/100, self.timer_callback)
+        self.timer = self.create_timer(1/300, self.timer_callback)
 
         self.move_service = self.create_service(Move, "move", self.move_callback)
-        self.move_traj_service = self.create_service(MoveTraj, "move_traj", self.move_traj_callback)
+        self.move_traj_service = self.create_service(TrajMove, "move_traj", self.move_traj_callback)
         self.load_square_service = self.create_service(Waypoint, "load_square", self.load_square_callback)
         self.load_diamond_service = self.create_service(Waypoint, "load_diamond", self.load_diamond_callback)
         self.track_service = self.create_service(Empty, "track", self.track_callback)
@@ -38,7 +38,7 @@ class Tracker(Node):
         self.tf_listener = TransformListener(self.buffer, self)
         self.static_tf_broadcaster = StaticTransformBroadcaster(self, qos)
 
-        self.ser = serial.Serial("/dec", 115200, timeout=0.01)
+        self.ser = serial.Serial("/dev/ttyACM0", 115200, timeout=0.01)
         self.queue = deque(maxlen=500)
         self.tracking_flag = [False]
         self.thread = threading.Thread(target=serial_read_thread,
@@ -46,55 +46,84 @@ class Tracker(Node):
                                        daemon=True)
         self.thread.start()
 
-        # Wait until the required frames exist
-        self.get_logger().info("Waiting for 'frame_tag' -> 'ee_tag' transform to appear...")
-        while rclpy.ok():
-            try:
-                # Timeout of 0 seconds just checks if it’s available right now
-                if self.buffer.can_transform('frame_tag', 'ee_tag', rclpy.time.Time(), rclpy.duration.Duration(seconds=0.1)):
-                    self.get_logger().info("'frame_tag' -> 'ee_tag' transform found!")
-                    break
-            except Exception:
-                pass
-            time.sleep(0.05)  # avoid busy-wait
+        # # Wait until the required frames exist
+        # self.get_logger().info("Waiting for transform 'frame_tag' -> 'ee_tag'...")
 
-        frame2center = TransformStamped()
-        frame2center.header.stamp = self.get_clock().now().to_msg()
-        frame2center.header.frame_id = 'frame_tag'
-        frame2center.child_frame_id = 'center'
-        frame2center.transform.translation.y = -.53965
-        frame2center.transform.translation.z = -.1052592
-        self.static_tf_broadcaster.sendTransform(frame2center)
-        self.get_logger().info("Static transform published from frame_tag -> center")
+        # while rclpy.ok():
+        #     try:
+        #         # latest transform
+        #         t = self.buffer.lookup_transform(
+        #             target_frame='frame_tag',
+        #             source_frame='ee_tag')
+        #         self.get_logger().info("Transform found!")
+        #         break
+        #     except Exception:
+        #         time.sleep(0.05)  # avoid busy wait
+        
+        # frame2center = TransformStamped()
+        # frame2center.header.stamp = self.get_clock().now().to_msg()
+        # frame2center.header.frame_id = 'frame_tag'
+        # frame2center.child_frame_id = 'center'
+        # frame2center.transform.translation.y = -.53965
+        # frame2center.transform.translation.z = -.1052592
+        # self.static_tf_broadcaster.sendTransform(frame2center)
+        # self.get_logger().info("Static transform published from frame_tag -> center")
 
         self.ee_pos_array = np.empty((0, 2))
         self.apriltag_pos_array = np.empty((0, 2))
         self.waypoints = np.empty((0, 2))
         self.use_traj = False
+        self.center_frame_defined = False
 
     def timer_callback(self):
-        if not self.tracking_flag[0]:
-            return
+        if not self.center_frame_defined:
+            self.get_logger().error("Waiting for center frame to be defined")
+            try:
+                t = self.buffer.lookup_transform(
+                    'frame_tag',
+                    'ee_tag',
+                    rclpy.time.Time()
+                )
+                self.get_logger().info("Transform found! Publishing static transform...")
 
-        while not self.queue.empty():
-            line = self.queue.get_nowait()
-            if line == "done":
-                self.stop_tracking()
-            else:
-                try:
-                    x_str, y_str = line.split(",")
-                    x, y = float(x_str), float(y_str)
-                    self.ee_pos_array = np.vstack([self.ee_pos_array, np.array([x, y])])
-                except Exception:
-                    self.get_logger().warn(f"Invalid serial line: {line}")
-        
-        try:
-            transform = self.buffer.lookup_transform("center", "ee_tag", rclpy.time.Time())
-            x = transform.transform.translation.x
-            y = transform.transform.translation.y
-            self.apriltag_pos_array = np.vstack([self.apriltag_pos_array, np.array([x, y])])
-        except Exception:
-            pass
+                frame2center = TransformStamped()
+                frame2center.header.stamp = self.get_clock().now().to_msg()
+                frame2center.header.frame_id = 'frame_tag'
+                frame2center.child_frame_id = 'center'
+                frame2center.transform.translation.x = 0.0
+                frame2center.transform.translation.y = -0.53965
+                frame2center.transform.translation.z = -0.1052592
+                frame2center.transform.rotation.w = 1.0
+                self.static_tf_broadcaster.sendTransform(frame2center)
+
+                self.center_frame_defined = True
+
+            except TransformException:
+                pass  # still waiting
+        else:
+            if not self.tracking_flag[0]:
+                return
+
+            while len(self.queue) > 0:
+                line = self.queue.popleft()
+                if line == "done":
+                    self.stop_tracking()
+                else:
+                    try:
+                        x_str, y_str = line.split(",")
+                        x, y = float(x_str), float(y_str)
+                        self.ee_pos_array = np.vstack([self.ee_pos_array, np.array([x, y])])
+                    except Exception:
+                        self.get_logger().warn(f"Invalid serial line: {line}")
+            
+            try:
+                transform = self.buffer.lookup_transform("center", "ee_tag", rclpy.time.Time(),
+                                                         timeout=rclpy.time.Duration(seconds=0.001))
+                x = transform.transform.translation.x
+                y = transform.transform.translation.y
+                self.apriltag_pos_array = np.vstack([self.apriltag_pos_array, np.array([x, y])])
+            except Exception:
+                pass
 
     def move_callback(self, request, response):
         x = request.x
@@ -162,7 +191,7 @@ class Tracker(Node):
         self.start_tracking()
         return response
     
-    def plot_callback(self):
+    def plot_callback(self, reqest, response):
         plt.plot(self.waypoints[:, 0], self.waypoints[:, 1], '-ok')
         plt.plot(self.ee_pos_array[:, 0], self.ee_pos_array[:, 1], '-r')
         plt.plot(self.apriltag_pos_array[:, 0], self.apriltag_pos_array[:, 1], '-b')
@@ -173,6 +202,8 @@ class Tracker(Node):
             plt.title("EE Movement Performance - Trajectory Control")
         else:
             plt.title("EE Movement Performance - Position Control")
+        plt.show()
+        return response
 
 
     def start_tracking(self):
